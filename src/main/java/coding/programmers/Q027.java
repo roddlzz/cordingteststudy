2efd4cd6a68c0c04f0239bb2256fba59dbd6b166
양은경 <eunkyung.yang@evidnet.co.kr>
2024-10-07 18:08:46 +0900

    241007
    Signed-off-by: 양은경 <eunkyung.yang@evidnet.co.kr>

--- src/main/java/coding/programmers/Q027.java
+++ src/main/java/coding/programmers/Q027.java
@@ -1,6 +1,8 @@
 package coding.programmers;
 
-//  배열 만들기4 https://school.programmers.co.kr/learn/courses/30/lessons/181918 Todo
+import java.util.Stack;
+
+//  배열 만들기4 https://school.programmers.co.kr/learn/courses/30/lessons/181918
 //  정수 배열 arr가 주어집니다. arr를 이용해 새로운 배열 stk를 만드려고 합니다.
 //  변수 i를 만들어 초기값을 0으로 설정한 후 i가 arr의 길이보다 작으면 다음 작업을 반복합니다.
 //  만약 stk가 빈 배열이라면 arr[i]를 stk에 추가하고 i에 1을 더합니다.
@@ -24,13 +26,19 @@ package coding.programmers;
 //    -	-	[1, 2, 3]
 //    따라서 [1, 2, 3]을 return 합니다.
 public class Q027 {
-//  public int[] solution(int[] arr) {
-//    int[] stk = {};
-//    for (int i =0; i<arr.length;i++){
-//      if (stk == null){
-//        stk = stk + arr[i];
-//      }
-//    }
-//    return stk;
-//  }
+  public int[] solution(int[] arr) {
+    Stack<Integer> stk = new Stack<>();
+    int i = 0;
+
+    while (i < arr.length) {
+      if (stk.isEmpty() || arr[i] > stk.peek()) {
+        stk.add(arr[i]);
+        i++;
+      } else {
+        stk.pop();
+      }
+    }
+
+    return stk.stream().mapToInt(num -> num).toArray();
+  }
 }
--- src/main/java/coding/programmers/Q029.java
+++ src/main/java/coding/programmers/Q029.java
@@ -1,8 +1,11 @@
 package coding.programmers;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-//  주사위게임3 Todo 피드백7 받았는데 다시 질문
+//  주사위게임3 https://school.programmers.co.kr/learn/courses/30/lessons/181916
 //  1부터 6까지 숫자가 적힌 주사위가 네 개 있습니다. 네 주사위를 굴렸을 때 나온 숫자에 따라 다음과 같은 점수를 얻습니다.
 //  네 주사위에서 나온 숫자가 모두 p로 같다면 1111 × p점을 얻습니다.
 //  세 주사위에서 나온 숫자가 p로 같고 나머지 다른 주사위에서 나온 숫자가 q(p ≠ q)라면 (10 × p + q)2 점을 얻습니다.
@@ -19,74 +22,32 @@ import java.util.HashMap;
 //       * 6	4	2	5	2
 public class Q029 {
   public int solution(int a, int b, int c, int d) {
-    int answer = 0;
-    HashMap<Integer, Integer> cnt = new HashMap<>();
-    int[] keyArray = {a, b, c, d};
+    List<Integer> numbers = Arrays.asList(a, b, c, d);
+    numbers.sort(Integer::compareTo);
 
-//    key의 값이 있으면 count +1 해서 같은 값이 있는 key의 count를 담음
-    for (int key : keyArray) {
-      if (cnt.containsKey(key)) {
-        Integer value = cnt.get(key);
-        cnt.put(key, value + 1);
-      } else {
-        cnt.put(key, 1);
-      }
+    Map<Integer, Integer> numberCountmap = new HashMap<>();
+
+    for (int number : numbers) {
+      numberCountmap.put(number, numberCountmap.getOrDefault(number, 0) + 1);
     }
 
-//    모든 값이 같은 주사위일때
-    if (cnt.size() == 1) {
-      answer = 1111 * a;
+    int answer = 0;
+    int min = numberCountmap.get(a);
 
-//    주사위 값이 두가지가 있을 경우
-    } else if (cnt.size() == 2) {
-//    1. 주사위가 두 개씩 같은 값이 나오고, 나온 숫자를 각각 p, q(p ≠ q)라고 한다면 (p + q) × |p - q|점을 얻습니다.
-//    2. 세 주사위에서 나온 숫자가 p로 같고 나머지 다른 주사위에서 나온 숫자가 q(p ≠ q)라면 (10 × p + q)제곱 점
-      int p = 0;
-      int q = 0;
-//      1번 경우
-      for (int key : cnt.keySet()) {
-        if (cnt.get(key) == 2) {
-          p = key;
-        } else {
-          q = key;
-        }
-      }
-//      2번 경우
-      if (cnt.get(p) == 3) {
-        answer = (10 * p + q) * (10 * p + q);
+    if (numberCountmap.size() == 1) {
+      answer = 1111 * min;
+    } else if (numberCountmap.size() == 2) {
+//  세 주사위에서 나온 숫자가 p로 같고 나머지 다른 주사위에서 나온 숫자가 q(p ≠ q)라면 (10 × p + q)2 점을 얻습니다.
+//  주사위가 두 개씩 같은 값이 나오고, 나온 숫자를 각각 p, q(p ≠ q)라고 한다면 (p + q) × |p - q|점을 얻습니다.
+      if (numberCountmap.containsValue(3)){
+        answer = numberCountmap.;
       } else {
-        answer = (p + q) * (p - q);
+        answer = (min + numbers.get(1)) * Math.abs(min - numbers.get(1));
       }
+    } else if (numberCountmap.size() == 3) {
 
-//      주사위 값이 3개 있을 때
-    } else if (cnt.size() == 3) {
-//      어느 두 주사위에서 나온 숫자가 p로 같고 나머지 두 주사위에서 나온 숫자가 각각 p와 다른 q, r(q ≠ r)이라면 q × r점을
-      int p = 0;
-      int q = 0;
-      int r = 0;
-
-      for (int key : cnt.keySet()) {
-        if (cnt.get(key) == 2) {
-          p = key;
-        } else if (q == 0) {
-          q = key;
-        } else {
-          r = key;
-        }
-      }
-      answer = q * r;
-
-//      모두 다른 값일때
     } else {
-      int min = a;
-      if (b < a) {
-        min = b;
-      } else if (c < b) {
-        min = c;
-      } else if (d < c) {
-        min = d;
-      }
-      answer = min;
+      answer = numberCountmap.get(min);
     }
 
     return answer;
