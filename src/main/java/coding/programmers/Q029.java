3988019a9cc9d7bbdede8cb29bc55d4e27606e05
양은경 <eunkyung.yang@evidnet.co.kr>
2024-10-16 17:06:35 +0900

    241016
    Signed-off-by: 양은경 <eunkyung.yang@evidnet.co.kr>

--- src/main/java/coding/programmers/Q029.java
+++ src/main/java/coding/programmers/Q029.java
@@ -22,35 +22,67 @@ import java.util.Map;
 //       * 6	4	2	5	2
 public class Q029 {
   public int solution(int a, int b, int c, int d) {
-    List<Integer> numbers = Arrays.asList(a, b, c, d);
-    numbers.sort(Integer::compareTo);
+    int[] numbers = {a, b, c, d};
 
+    // 키 = number, 값 = number count
     Map<Integer, Integer> numberCountmap = new HashMap<>();
 
     for (int number : numbers) {
       numberCountmap.put(number, numberCountmap.getOrDefault(number, 0) + 1);
     }
 
-    int answer = 0;
-    int min = numberCountmap.get(a);
+    int p = 0;
+    int q = 0;
+    int r = 0;
 
+    // 네 주사위에서 나온 숫자가 모두 p로 같다면 1111 × p점
     if (numberCountmap.size() == 1) {
-      answer = 1111 * min;
-    } else if (numberCountmap.size() == 2) {
-//  세 주사위에서 나온 숫자가 p로 같고 나머지 다른 주사위에서 나온 숫자가 q(p ≠ q)라면 (10 × p + q)2 점을 얻습니다.
-//  주사위가 두 개씩 같은 값이 나오고, 나온 숫자를 각각 p, q(p ≠ q)라고 한다면 (p + q) × |p - q|점을 얻습니다.
-      if (numberCountmap.containsValue(3)){
-        answer = numberCountmap.;
-      } else {
-        answer = (min + numbers.get(1)) * Math.abs(min - numbers.get(1));
-      }
-    } else if (numberCountmap.size() == 3) {
-
-    } else {
-      answer = numberCountmap.get(min);
+      return 1111 * a;
     }
 
-    return answer;
+    if (numberCountmap.size() == 2) {
+      // 세 주사위에서 나온 숫자가 p로 같고 나머지 다른 주사위에서 나온 숫자가 q(p ≠ q)라면 (10 × p + q)2 점을 얻습니다.
+      if (numberCountmap.containsValue(3)) {
+        for (Map.Entry<Integer, Integer> entry : numberCountmap.entrySet()) {
+          if (entry.getValue() == 3) {
+            p = entry.getKey();
+          } else {
+            q = entry.getKey();
+          }
+        }
+        return (10 * p + q) * (10 * p + q);
+      }
+
+      // 주사위가 두 개씩 같은 값이 나오고, 나온 숫자를 각각 p, q(p ≠ q)라고 한다면 (p + q) × |p - q|점을 얻습니다.
+      if (numberCountmap.containsValue(2)) {
+        int[] keys = new int[2];
+        int index = 0;
+        for (int key : numberCountmap.keySet()) {
+          keys[index++] = key;
+        }
+        p = keys[0];
+        q = keys[1];
+        return (p + q) * Math.abs(p - q);
+      }
+    }
+
+    // 어느 두 주사위에서 나온 숫자가 p로 같고 나머지 두 주사위에서 나온 숫자가 각각 p와 다른 q, r(q ≠ r)이라면 q × r점
+    if (numberCountmap.size() == 3) {
+      for (Map.Entry<Integer, Integer> entry : numberCountmap.entrySet()) {
+        if (entry.getValue() == 2) {
+          p = entry.getKey();
+        } else if (q == 0) {
+          q = entry.getKey();
+        } else {
+          r = entry.getKey();
+        }
+      }
+      return q * r;
+    }
+
+    // 네 주사위에 적힌 숫자가 모두 다르다면 나온 숫자 중 가장 작은 숫자 만큼의 점수
+    Arrays.sort(numbers);
+    return numbers[0];
   }
 }
 
