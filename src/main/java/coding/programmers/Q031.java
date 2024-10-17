f740607d6686f519e8933c99adf379fcb529f97d
양은경 <eunkyung.yang@evidnet.co.kr>
2024-10-17 15:41:56 +0900

    241017
    Signed-off-by: 양은경 <eunkyung.yang@evidnet.co.kr>

--- src/main/java/coding/programmers/Q031.java
+++ src/main/java/coding/programmers/Q031.java
@@ -1,11 +1,19 @@
 package coding.programmers;
 
-//  9로 나눈 나머지  Todo 피드백 받았는데 다시 질문
+//  9로 나눈 나머지 https://school.programmers.co.kr/learn/courses/30/lessons/181914
 //  음이 아닌 정수를 9로 나눈 나머지는 그 정수의 각 자리 숫자의 합을 9로 나눈 나머지와 같은 것이 알려져 있습니다.
 //  이 사실을 이용하여 음이 아닌 정수가 문자열 number로 주어질 때, 이 정수를 9로 나눈 나머지를 return 하는 solution 함수를 작성해주세요.
+//  입출력 예
+//  number	result
+//  "123"	6
+//  "78720646226947352489"	2
+
 public class Q031 {
   public int solution(String number) {
-    int num = Integer.parseInt(number);
-    return num % 9;
+    int answer = 0;
+    for (int i = 0; i < number.length(); i++) {
+      answer += number.charAt(i) - '0'; // 각 자리 숫자를 더함
+    }
+    return answer % 9;
   }
 }
--- src/main/java/coding/programmers/Q032.java
+++ src/main/java/coding/programmers/Q032.java
@@ -1,22 +1,39 @@
 package coding.programmers;
 
-//  문자열 여러 번 뒤집기  todo
+//  문자열 여러 번 뒤집기 https://school.programmers.co.kr/learn/courses/30/lessons/181913
 //  문자열 my_string과 이차원 정수 배열 queries가 매개변수로 주어집니다. queries의 원소는 [s, e] 형태로, my_string의 인덱스 s부터 인덱스 e까지를 뒤집으라는 의미입니다.
 //  my_string에 queries의 명령을 순서대로 처리한 후의 문자열을 return 하는 solution 함수를 작성해 주세요.
+//  입출력 예
+//  my_string	queries	result
+//  "rermgorpsam"	[[2, 3], [0, 7], [5, 9], [6, 10]]	"programmers"
+
 public class Q032 {
   public String solution(String my_string, int[][] queries) {
     String answer = "";
-    for (int i = 0; i < queries.length; i++) {
-      int s = queries[i][0];
-      int e = queries[i][1];
+    String[] strings = new String[my_string.length()];
 
-//      String tmp = my_string.substring(s, e+1);
-
-      for (int j =e; j <= s; j--){
-
-      }
-
+    for (int i = 0; i < my_string.length(); i++) {
+      strings[i] = my_string.charAt(i) + "";
     }
+
+    for (int i = 0; i < queries.length; i++) {
+      int start = queries[i][0];
+      int end = queries[i][1];
+
+      // 문자열 뒤집기
+      while (start < end) {
+        String temp = strings[start];
+        strings[start] = strings[end];
+        strings[end] = temp;
+        start++;
+        end--;
+      }
+    }
+
+    for (int i = 0; i < strings.length; i++) {
+      answer += strings[i];
+    }
+
     return answer;
   }
 }
